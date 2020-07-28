b513319ba34eecbbbb9afea1f689e5a3b5228043
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2020-07-29 02:24:39 +0700

    update - user model migration

--- database/migrations/2020_07_28_191100_create_user_biodatas_table.php
+++ database/migrations/2020_07_28_191100_create_user_biodatas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserBiodatasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_biodatas', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('code')->unique();
-            $table->string('active');
+            $table->string('code');
+            $table->string('name');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
-            $table->softDeletes();
         });
     }
 
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_biodatas');
     }
 }
