043881edb62d9f017bb40c0fb92d6645fd6f06ba
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2020-07-29 03:49:17 +0700

    create - middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'useractive' => \App\Http\Middleware\UserActiveStatus::class,
+        'checkrole' => \App\Http\Middleware\CheckRole::class,
     ];
 }
