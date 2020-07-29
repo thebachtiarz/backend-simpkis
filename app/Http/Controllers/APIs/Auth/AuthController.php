46e09129e906239a3d736a4ef415f9bb56057ede
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2020-07-29 13:51:49 +0700

    update - create auth controller method

---
+++ app/Http/Controllers/APIs/Auth/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\APIs\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth:sanctum'], ['only' => ['credential', 'logout']]);
+    }
+
+    public function login()
+    {
+        $validator = Validator(request()->all(), [
+            'username' => 'required|string|alpha_num',
+            'password' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(errorResponse($validator->errors()), 202);
+        }
+        if (Auth::attempt(request(['username', 'password']))) {
+            if (User_isActive(Auth::user()->code)) {
+                return $this->respondWithToken(Auth::user()->createToken(Auth::user()->username . '-' . getClientIpAddress())->plainTextToken);
+            }
+            return response()->json(errorResponse('Your account has been ' . User_getActiveStatus(Auth::user()->active) . ' due to bad behavior.'), 202);
+        }
+        return response()->json(errorResponse('Account not found !'), 202);
+    }
+
+    public function logout()
+    {
+        if (request('_action') == 'revoke') {
+            if (Auth::user()->tokens()->delete()) {
+                return response()->json(successResponse('Successfully Revoke All Logins'), 201);
+            }
+        } else {
+            if (Auth::user()->currentAccessToken()->delete()) {
+                return response()->json(successResponse('Successfully Logout'), 201);
+            }
+        }
+        return response()->json(errorResponse('Failed to Logout'), 202);
+    }
+
+    public function credential()
+    {
+        return response()->json(dataResponse(['name' => Auth::user()->userbio->name]), 200);
+    }
+
+    # protected
+    protected function respondWithToken($token)
+    {
+        return response()->json(dataResponse([
+            'account_name' => Auth::user()->userbio->name,
+            'status' => User_getStatusForHuman(Auth::user()->userstat->status),
+            'access_token' => $token,
+            'token_type' => 'bearer'
+        ], '', 'Authorization'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * jwt auth default route
+ * access : /api/auth/---
+ */
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', 'APIs\Auth\AuthController@login');
+    Route::post('/logout', 'APIs\Auth\AuthController@logout');
+    Route::get('/creds', 'APIs\Auth\AuthController@credential');
 });
