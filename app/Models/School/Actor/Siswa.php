edb9e4fef75a5f5013dfac5ea71efc4896657859
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2020-08-05 05:45:34 +0700

    create - kelas siswa ketuakelas models

---
+++ app/Models/School/Actor/Siswa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\School\Actor;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    //
+}
---
+++ app/Models/School/Curriculum/Kelas.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\School\Curriculum;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    //
+}
